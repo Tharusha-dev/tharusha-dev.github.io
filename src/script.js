5e22f9f7f93ea03e43b3a0b290642b7c8cfac078
Tharusha-dev <tharushajayasooriya.dev004@gmail.com>
2022-03-19 17:25:05 +0000

    added creations (project) page

--- src/script.js
+++ src/script.js
@@ -1,5 +1,8 @@
 let navItems = document.getElementsByClassName("nav-item")
 let navItemTextBottom = document.getElementsByClassName("nav-item-text-bottom")
+let projectImageOverlayWrapper = document.getElementsByClassName("project-image-overlay-wrapper")
+let projectImageOverlay = document.getElementsByClassName("project-image-overlay")
+let test = document.getElementById("tets") 
 
 navItems[0].addEventListener("mouseover",()=>{
     navItemTextBottom[0].classList.add("nav-item-animate-bottom")
@@ -8,3 +11,9 @@ navItems[0].addEventListener("mouseover",()=>{
 navItems[0].addEventListener("mouseout",()=>{
     navItemTextBottom[0].classList.remove("nav-item-animate-bottom")
 })
+
+projectImageOverlayWrapper[0].addEventListener("mouseover",()=>{projectImageOverlay[0].style.display = "grid";projectImageOverlay[0].style.pointerEvents = "all"})
+projectImageOverlayWrapper[0].addEventListener("mouseout",()=>{projectImageOverlay[0].style.display = "none"})
+
+
+test.style.pointerEvents
