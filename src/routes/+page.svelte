a965d737c885edb69ec38c40afe56e74c7ba71f1
Tharusha-dev <tharushajayasooriya.dev004@gmail.com>
2024-08-03 16:47:11 +0530

    animated articles section, changed favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,6 +16,7 @@
 
 	// @ts-ignore
 	let leftInDivs;
+	let topIn;
 
 	// @ts-ignore
 	let rightInDivs;
@@ -34,6 +35,7 @@ homeNavButton = document.getElementById('nav-home');
 		portfolioNavButton = document.getElementById('nav-portfolio');
 
 		leftInDivs = document.querySelectorAll('.in-left');
+		topIn = document.querySelectorAll('.in-top');
 
 		rightInDivs = document.querySelectorAll('.in-right');
 
@@ -48,19 +50,36 @@ homeNavButton = document.getElementById('nav-home');
 		for (var i = 0; i < leftInDivs.length; i++) {
 			// @ts-ignore
 			var leftIn = leftInDivs[i];
-			// @ts-ignore
 			var rightIn = rightInDivs[i];
 
 			var distInView = leftIn.getBoundingClientRect().top - window.innerHeight + 20;
 			if (distInView < 0) {
 				try {
-					leftIn.classList.add('in-right-animate');
-					rightIn.classList.add('in-right-animate');
+					leftIn.classList.add('in-animate');
+					rightIn.classList.add('in-animate');
 				} catch (err) {}
 			} else {
 				try {
-					leftIn.classList.remove('in-right-animate');
-					rightIn.classList.remove('in-right-animate');
+					leftIn.classList.remove('in-animate');
+					rightIn.classList.remove('in-animate');
+				} catch (err) {}
+			}
+		}
+
+		for (var i = 0; i < topIn.length; i++) {
+	
+			var topInEl = topIn[i];
+
+			var distInView = topInEl.getBoundingClientRect().top - window.innerHeight;
+			if (distInView < 0) {
+				try {
+					topInEl.classList.add('in-animate');
+					
+				} catch (err) {}
+			} else {
+				try {
+					topInEl.classList.remove('in-animate');
+					
 				} catch (err) {}
 			}
 		}
@@ -1090,46 +1109,50 @@ homeNavButton = document.getElementById('nav-home');
 				</div>
 			</div>
 		</div>
-		<div class="article">
-			<h1>Article</h1>
-			<h2>How search engines work, The absolute basics of indexing and retrieval</h2>
-			<a
-				href="https://tharushajayasooriya.medium.com/how-search-engines-work-the-absolute-basics-of-indexing-and-retrieval-7ff3f084cb4c"
-				target="_blank"
-			>
-				<button> Read more </button>
-			</a>
+
+		<div class="articles">
+			<div class="article in-top">
+				<h1>Article</h1>
+				<h2>How search engines work, The absolute basics of indexing and retrieval</h2>
+				<a
+					href="https://tharushajayasooriya.medium.com/how-search-engines-work-the-absolute-basics-of-indexing-and-retrieval-7ff3f084cb4c"
+					target="_blank"
+				>
+					<button> Read more </button>
+				</a>
+			</div>
+			<div class="article in-top">
+				<h1>Article</h1>
+				<h2>Create an Apple like ecosystem with Linux and Android</h2>
+				<a
+					href="https://medium.com/@tharushajayasooriya/create-an-apple-like-ecosystem-with-linux-and-android-8b69e6941dc9"
+					target="_blank"
+				>
+					<button> Read more </button>
+				</a>
+			</div>
+			<div class="article in-top">
+				<h1>Article</h1>
+				<h2>TryHackMe OWSAP juice shop deep dive. More than a CTF Walkthrough</h2>
+				<a
+					href="https://medium.com/@tharushajayasooriya/tryhackme-owsap-juice-shop-deep-dive-more-than-a-ctf-walkthrough-de598fd6cc24"
+					target="_blank"
+				>
+					<button> Read more </button>
+				</a>
+			</div>
+			<div class="article in-top">
+				<h1>Article</h1>
+				<h2>Set up animated wallpapers on Linux (Gnome/KDE)</h2>
+				<a
+					href="https://medium.com/@tharushajayasooriya/how-to-get-live-wallpapers-on-linux-gnome-kde-c52282d719fa"
+					target="_blank"
+				>
+					<button> Read more </button>
+				</a>
+			</div>
 		</div>
-		<div class="article">
-			<h1>Article</h1>
-			<h2>Create an Apple like ecosystem with Linux and Android</h2>
-			<a
-				href="https://medium.com/@tharushajayasooriya/create-an-apple-like-ecosystem-with-linux-and-android-8b69e6941dc9"
-				target="_blank"
-			>
-				<button> Read more </button>
-			</a>
-		</div>
-		<div class="article">
-			<h1>Article</h1>
-			<h2>TryHackMe OWSAP juice shop deep dive. More than a CTF Walkthrough</h2>
-			<a
-				href="https://medium.com/@tharushajayasooriya/tryhackme-owsap-juice-shop-deep-dive-more-than-a-ctf-walkthrough-de598fd6cc24"
-				target="_blank"
-			>
-				<button> Read more </button>
-			</a>
-		</div>
-		<div class="article">
-			<h1>Article</h1>
-			<h2>Set up animated wallpapers on Linux (Gnome/KDE)</h2>
-			<a
-				href="https://medium.com/@tharushajayasooriya/how-to-get-live-wallpapers-on-linux-gnome-kde-c52282d719fa"
-				target="_blank"
-			>
-				<button> Read more </button>
-			</a>
-		</div>
+
 	</section>
 	
 
@@ -1335,7 +1358,7 @@ homeNavButton = document.getElementById('nav-home');
 			<div class="about-item-outer about-item-outer-right about-item-outer-contact">
 				<div class="about-dot about-dot-right large-screen-only"></div>
 				<div class="about-item-text-right">
-					<span> Contact me </span>
+					<span> Socials </span>
 					<div class="about-item-logo">
 						<svg viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<circle cx="37" cy="37" r="36.5" stroke="white" />
@@ -1348,7 +1371,7 @@ homeNavButton = document.getElementById('nav-home');
 				</div>
 
 				<div class="contacts">
-					<a href="mailto:tharushamjayasooriya@gmail.com" class="contact in-right">
+					<a href="mailto:tharushamjayasooriya@gmail.com" target="_blank" class="contact in-right">
 						<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<path
 								d="M4 7.00005L10.2 11.65C11.2667 12.45 12.7333 12.45 13.8 11.65L20 7"
@@ -1369,6 +1392,7 @@ homeNavButton = document.getElementById('nav-home');
 							/>
 						</svg>
 					</a>
+			
 					<a href="https://linkedin.com/in/tharushaj" target="_blank" class="contact in-right">
 						<svg viewBox="0 0 192 192" xmlns="http://www.w3.org/2000/svg" fill="none">
 							<rect
@@ -1395,6 +1419,36 @@ homeNavButton = document.getElementById('nav-home');
 								d="M126 130v-26c0-9.941-8.059-18-18-18v0c-9.941 0-18 8.059-18 18v26"
 							/>
 						</svg>
+					</a>
+
+					<a  href="https://github.com/tharusha-dev" target="_blank" class="contact in-right">
+						<svg 
+						style="width: 75%;"
+						viewBox="0 0 20 20"
+						version="1.1"
+						xmlns="http://www.w3.org/2000/svg"
+						xmlns:xlink="http://www.w3.org/1999/xlink"
+					>
+						<g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+							<g
+								id="Dribbble-Light-Preview"
+								transform="translate(-140.000000, -7559.000000)"
+								fill="#ffffff"
+							>
+								<g id="icons" transform="translate(56.000000, 160.000000)">
+									<path
+										d="M94,7399 C99.523,7399 104,7403.59 104,7409.253 C104,7413.782 101.138,7417.624 97.167,7418.981 C96.66,7419.082 96.48,7418.762 96.48,7418.489 C96.48,7418.151 96.492,7417.047 96.492,7415.675 C96.492,7414.719 96.172,7414.095 95.813,7413.777 C98.04,7413.523 100.38,7412.656 100.38,7408.718 C100.38,7407.598 99.992,7406.684 99.35,7405.966 C99.454,7405.707 99.797,7404.664 99.252,7403.252 C99.252,7403.252 98.414,7402.977 96.505,7404.303 C95.706,7404.076 94.85,7403.962 94,7403.958 C93.15,7403.962 92.295,7404.076 91.497,7404.303 C89.586,7402.977 88.746,7403.252 88.746,7403.252 C88.203,7404.664 88.546,7405.707 88.649,7405.966 C88.01,7406.684 87.619,7407.598 87.619,7408.718 C87.619,7412.646 89.954,7413.526 92.175,7413.785 C91.889,7414.041 91.63,7414.493 91.54,7415.156 C90.97,7415.418 89.522,7415.871 88.63,7414.304 C88.63,7414.304 88.101,7413.319 87.097,7413.247 C87.097,7413.247 86.122,7413.234 87.029,7413.87 C87.029,7413.87 87.684,7414.185 88.139,7415.37 C88.139,7415.37 88.726,7417.2 91.508,7416.58 C91.513,7417.437 91.522,7418.245 91.522,7418.489 C91.522,7418.76 91.338,7419.077 90.839,7418.982 C86.865,7417.627 84,7413.783 84,7409.253 C84,7403.59 88.478,7399 94,7399"
+										id="github-[#142]"
+									>
+									</path>
+								</g>
+							</g>
+						</g>
+					</svg>
+					</a>
+
+					<a href="https://medium.com/@tharushajayasooriya" target="_blank" class="contact in-right">
+						<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M13 12C13 15.3137 10.3137 18 7 18C3.68629 18 1 15.3137 1 12C1 8.68629 3.68629 6 7 6C10.3137 6 13 8.68629 13 12Z" fill="#ffffff"></path> <path d="M23 12C23 14.7614 22.5523 17 22 17C21.4477 17 21 14.7614 21 12C21 9.23858 21.4477 7 22 7C22.5523 7 23 9.23858 23 12Z" fill="#ffffff"></path> <path d="M17 18C18.6569 18 20 15.3137 20 12C20 8.68629 18.6569 6 17 6C15.3431 6 14 8.68629 14 12C14 15.3137 15.3431 18 17 18Z" fill="#ffffff"></path> </g></svg>
 					</a>
 				</div>
 			</div>
